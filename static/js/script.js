11316f21dd48406a35be0da20c66f237949cfda1
Isaaq Guleed <st0lenstar@icloud.com>
2021-11-16 18:24:57 +0100

    fixed close cmd locally

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  function winclose() {
-    var gui = require('nw.gui');
-    var win = gui.Window.get();
-    win.close();
-  }
   var text = document.getElementById("text");
   while (text.firstChild) text.removeChild(text.firstChild);
   document.getElementById("text").innerHTML += locations.start.desc;
@@ -22,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
           document.getElementById("text").innerHTML += locations.start.desc;
           break;
         case "close":
-          winclose();
+          window.close();
           break;
       }
       document.getElementById("usr-input").value = "";
