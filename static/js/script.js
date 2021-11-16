dfa0d29bad7ce5f0278efc888bd83783336b35bf
Isaaq Guleed <st0lenstar@icloud.com>
2021-11-16 18:15:27 +0100

    testing to see if closing works, deleted jquery

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
   var text = document.getElementById("text");
   while (text.firstChild) text.removeChild(text.firstChild);
-  document.getElementById("text").innerHTML += (locations.start.desc);
+  document.getElementById("text").innerHTML += locations.start.desc;
   document.addEventListener("keydown", enter, false);
   function enter(e) {
     var keyCode = e.keyCode;
     if (keyCode == 13) {
-        var input = document.getElementById("usr-input").value.toLowerCase();
-        switch (input) {
-            case "north":
-                while (text.firstChild) text.removeChild(text.firstChild);
-                document.getElementById("text").innerHTML +=(locations.north1.desc);
-                break;
-            case "reset":
-                while (text.firstChild) text.removeChild(text.firstChild);
-                document.getElementById("text").innerHTML +=(locations.start.desc);
-                break;
-            case "close":
-                window.close()
-                break;
-        };
-        document.getElementById("usr-input").value=""
+      var input = document.getElementById("usr-input").value.toLowerCase();
+      switch (input) {
+        case "north":
+          while (text.firstChild) text.removeChild(text.firstChild);
+          document.getElementById("text").innerHTML += locations.north1.desc;
+          break;
+        case "reset":
+          while (text.firstChild) text.removeChild(text.firstChild);
+          document.getElementById("text").innerHTML += locations.start.desc;
+          break;
+        case "close":
+          Window.close();
+          break;
+      }
+      document.getElementById("usr-input").value = "";
     }
   }
 });
