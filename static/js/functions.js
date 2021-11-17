546a056fb92358c6433b2a7c62cfb96b21917590
Isaaq Guleed <st0lenstar@icloud.com>
2021-11-17 18:34:10 +0100

    Trying to add play button on mobile

--- static/js/functions.js
+++ static/js/functions.js
@@ -36,6 +36,14 @@ function isAlive() {
   }
 }
 
+function isAlive() {
+  if (alive == true) {
+    document.getElementById("audio").play();
+  } else {
+    document.getElementById("death-audio").play();
+  }
+}
+
 function death() {
   alive = new Boolean(false);
 }
@@ -76,20 +84,3 @@ function muteDeath() {
     i2 -= 1;
   }
 }
-
-window.addEventListener("load", function () {
-  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  var source = audioCtx.createBufferSource();
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "audio-autoplay.wav");
-  xhr.responseType = "arraybuffer";
-  xhr.addEventListener("load", function (r) {
-    audioCtx.decodeAudioData(xhr.response, function (buffer) {
-      source.buffer = buffer;
-      source.connect(audioCtx.destination);
-      source.loop = false;
-    });
-    source.start(0);
-  });
-  xhr.send();
-});
