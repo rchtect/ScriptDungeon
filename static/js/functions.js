fcb03379d19109dd04e86e97138fa9e7892fcccc
Isaaq Guleed <st0lenstar@icloud.com>
2021-11-17 18:29:55 +0100

    testing hotfix audio play mobile

--- static/js/functions.js
+++ static/js/functions.js
@@ -76,3 +76,20 @@ function muteDeath() {
     i2 -= 1;
   }
 }
+
+window.addEventListener("load", function () {
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  var source = audioCtx.createBufferSource();
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "audio-autoplay.wav");
+  xhr.responseType = "arraybuffer";
+  xhr.addEventListener("load", function (r) {
+    audioCtx.decodeAudioData(xhr.response, function (buffer) {
+      source.buffer = buffer;
+      source.connect(audioCtx.destination);
+      source.loop = false;
+    });
+    source.start(0);
+  });
+  xhr.send();
+});
